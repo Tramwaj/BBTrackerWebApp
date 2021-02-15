f8b1daccd6532afae16933b8b436879b0500539c
Michał Salamon <ms055452@student.ath.edu.pl>
2021-02-15 18:36:51 +0100

    PlayerDTOValidator

---
+++ BBTracker.Contracts/Validators/PlayerDTOValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace BBTracker.Contracts.ViewModels
+{
+    public class PlayerDTOValidator : AbstractValidator<FullPlayerDTO>
+    {
+        public PlayerDTOValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().Length(3, 20);
+            RuleFor(x => x.Nick).NotEmpty().Length(2, 30);
+            RuleFor(x => x.City).NotEmpty().Length(2, 30);
+        }
+    }
+}
