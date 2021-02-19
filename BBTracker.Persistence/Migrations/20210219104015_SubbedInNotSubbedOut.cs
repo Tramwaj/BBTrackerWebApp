36fe2959f3a49c0b2c262c652f3bda695722f6ae
Michał Salamon <ms055452@student.ath.edu.pl>
2021-02-19 12:15:31 +0100

    change SubbedOut to SubbedIn

---
+++ BBTracker.Persistence/Migrations/20210219104015_SubbedInNotSubbedOut.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BBTracker.Persistence.Migrations
+{
+    public partial class SubbedInNotSubbedOut : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SubbedOut",
+                table: "Substitutions",
+                newName: "SubbedIn");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SubbedIn",
+                table: "Substitutions",
+                newName: "SubbedOut");
+        }
+    }
+}
