7a558042f7b32bc2f7f5f54aa7339e35b4e2b16e
Michał Salamon <ms055452@student.ath.edu.pl>
2021-02-19 12:14:30 +0100

    add On Courthandling

--- BBTracker.Persistence/Repos/SubstitutionRepo.cs
+++ BBTracker.Persistence/Repos/SubstitutionRepo.cs
@@ -15,9 +15,19 @@ namespace BBTracker.Persistence.Repos
         
         public async Task AddSubstitution(Substitution sub)
         {
+            sub.Player = await _context.Players.FirstAsync(p=>p.Id==sub.PlayerId);
             await _context.Substitutions.AddAsync(sub);
+            await _context.SaveChangesAsync();
         }
-        
+        public async Task<ICollection<Substitution>> GetPlayerSubstitutions(Guid playerId) => await _context.Substitutions
+            .Where(s => s.PlayerId == playerId)
+            .ToListAsync();
+
+        public async Task<ICollection<Substitution>> GetPlayerGameSubstitutions(Guid playerId,Guid gameId) => await _context.Substitutions
+            .Where(s => s.PlayerId == playerId && s.GameId == gameId)
+            .ToListAsync();
+
+
         public async Task<Substitution> GetLastSubByPlayerGame(Guid playerId, Guid gameId) => await
             _context.Substitutions.Where(s => s.Player.Id == playerId&& s.GameId==gameId).LastOrDefaultAsync();
         
