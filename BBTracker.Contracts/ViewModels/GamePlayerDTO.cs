d6b2648bc9c96e62bd77643596c534606400f27d
Michał Salamon <ms055452@student.ath.edu.pl>
2021-02-23 14:40:16 +0100

    move GamePlayerDTO to separate file

---
+++ BBTracker.Contracts/ViewModels/GamePlayerDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BBTracker.Contracts.ViewModels
+{
+    public class GamePlayerDTO
+    {
+        public Guid Id { get; set; }
+        public bool TeamB { get; set; }
+        public bool OnCourt { get; set; }
+    }
+}
--- BBTracker.Contracts/ViewModels/GamePlayersVM.cs
+++ BBTracker.Contracts/ViewModels/GamePlayersVM.cs
@@ -1,5 +1,4 @@
 ﻿using FluentValidation;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,12 +9,6 @@ namespace BBTracker.Contracts.ViewModels
     public class GamePlayersVM
     {
         public ICollection<GamePlayerDTO> Players { get; set; }
-    }
-    public class GamePlayerDTO
-    {
-        public Guid Id { get; set; }
-        public bool TeamB { get; set; }
-        public bool OnCourt { get; set; }
     }
     public class NewGamePlayersVMValidator : AbstractValidator<GamePlayersVM>
     {
