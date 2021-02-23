217fa4610b6ce0d03a6a0102653cc06746a61124
Michał Salamon <ms055452@student.ath.edu.pl>
2021-02-23 15:05:21 +0100

    add GameViewModel

---
+++ BBTracker.Contracts/ViewModels/GameViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBTracker.Contracts.ViewModels
+{
+    public class GameViewModel
+    {
+        public Guid Id { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        
+        public StatsDTO TeamAStats { get; set; }
+        public StatsDTO TeamBStats { get; set; }
+        public ICollection<StatsDTO> TeamA{ get; set; }
+        public ICollection<StatsDTO> TeamB{ get; set; }
+
+        public GameViewModel(Guid id, DateTime start, DateTime end, StatsDTO teamAStats, StatsDTO teamBStats, ICollection<StatsDTO> teamA, ICollection<StatsDTO> teamB)
+        {
+            Id = id;
+            Start = start;
+            End = end;
+            TeamAStats = teamAStats;
+            TeamBStats = teamBStats;
+            TeamA = teamA;
+            TeamB = teamB;
+        }
+        
+    }
+
+}
