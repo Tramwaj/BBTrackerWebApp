fe6168d56b442af9ba0e9027b0dee4c1bc4338c4
Michał Salamon <ms055452@student.ath.edu.pl>
2021-02-24 12:09:52 +0100

    move to App (will not be used outside of it)

---
+++ BBTracker.App/InterfacesApp/IPlayService.cs
@@ -0,0 +1,14 @@
+﻿using BBTracker.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBTracker.App.Interfaces
+{
+    public interface IPlayService
+    {
+        public Task<bool> PlayIsPossible(Play play);
+    }
+}
