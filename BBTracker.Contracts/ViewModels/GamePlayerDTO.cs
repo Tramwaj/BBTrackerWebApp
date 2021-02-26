3536a81de783707b8a994a026f94de5272ceea2d
Michał Salamon <ms055452@student.ath.edu.pl>
2021-02-26 10:48:17 +0100

    add validation

--- BBTracker.Contracts/ViewModels/GamePlayerDTO.cs
+++ BBTracker.Contracts/ViewModels/GamePlayerDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FluentValidation;
+using System;
 
 namespace BBTracker.Contracts.ViewModels
 {
@@ -7,5 +8,14 @@ namespace BBTracker.Contracts.ViewModels
         public Guid Id { get; set; }
         public bool TeamB { get; set; }
         public bool OnCourt { get; set; }
+    }
+    public class GamePlayerDTOValidator : AbstractValidator<GamePlayerDTO>
+    {
+        public GamePlayerDTOValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty();
+            RuleFor(x => x.TeamB).NotEmpty();
+            RuleFor(x => x.OnCourt).NotEmpty();
+        }
     }
 }
