d8eebf4563612edc5e1f693c0a2441a3391e08b5
Michał Salamon <ms055452@student.ath.edu.pl>
2021-09-21 22:26:34 +0200

    Add location

--- BBTracker.Model/Models/Location.cs
+++ BBTracker.Model/Models/Location.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace BBTracker.Model.Models
 {
-    class Location
+    public class Location
     {
+        public int Id { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public string OfficialName { get; set; }
+        public ICollection<string> OtherNames { get; set; }
+        public bool IsOutside { get; set; }
+
     }
 }
---
+++ BBTracker.Persistence/Configurations/LocationConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using BBTracker.Model.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBTracker.Persistence.Configurations
+{
+    public class LocationConfiguration : IEntityTypeConfiguration<Location>
+    {
+        public void Configure(EntityTypeBuilder<Location> builder)
+        {
+            builder.ToTable("Locations");
+            builder.HasKey(g => g.Id);
+            builder.Property(g => g.OfficialName).IsRequired();
+            builder.Property(g => g.City).IsRequired();
+            builder.Property(g => g.Street).IsRequired();
+        }
+    }
+}
