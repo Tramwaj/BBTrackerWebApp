dbeae117daeaab1bbb23d576b9d86a80ef901a3e
micsal <michal.salamon@bosmal.com.pl>
2022-04-12 15:25:19 +0200

    DI: singletons => transients

---
+++ BBTracker.Model/Models/PlayerTeam.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BBTracker.Model.Models
+{
+    public class PlayerTeam
+    {
+        public Player Player { get; set; }
+        public Team Team { get; set; }
+        public Guid PlayerId { get; set; }
+        public Guid TeamId { get; set; }
+
+    }
+}
