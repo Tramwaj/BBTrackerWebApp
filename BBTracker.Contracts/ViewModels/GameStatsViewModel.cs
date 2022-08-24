99a8630416019abed5a03407ef31ef91be791b8e
Michał Salamon <ms055452@student.ath.edu.pl>
2022-08-24 22:15:12 +0200

    Game stats with names

--- BBTracker.Contracts/ViewModels/GameStatsViewModel.cs
+++ BBTracker.Contracts/ViewModels/GameStatsViewModel.cs
@@ -11,23 +11,27 @@ namespace BBTracker.Contracts.ViewModels
         public Guid Id { get; set; }
         public DateTime Start { get; set; }
         public DateTime End { get; set; }
-        
+
         public StatsDTO TeamAStats { get; set; }
         public StatsDTO TeamBStats { get; set; }
-        public ICollection<StatsDTO> TeamA{ get; set; }
-        public ICollection<StatsDTO> TeamB{ get; set; }
+        public ICollection<StatsDTO> TeamA { get; set; }
+        public ICollection<StatsDTO> TeamB { get; set; }
 
-        public GameStatsViewModel(Guid id, DateTime start, DateTime end, StatsDTO teamAStats, StatsDTO teamBStats, ICollection<StatsDTO> teamA, ICollection<StatsDTO> teamB)
+        public GameStatsViewModel()
+        {
+
+        }
+        public GameStatsViewModel(Guid id, DateTime start, DateTime? end, StatsDTO teamAStats, StatsDTO teamBStats, ICollection<StatsDTO> teamA, ICollection<StatsDTO> teamB)
         {
             Id = id;
             Start = start;
-            End = end;
+            End = end ?? start;
             TeamAStats = teamAStats;
             TeamBStats = teamBStats;
             TeamA = teamA;
             TeamB = teamB;
         }
-        
+
     }
 
 }
