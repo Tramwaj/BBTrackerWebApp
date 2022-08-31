d2800f79b94310da86b509fed5b00a51906c05cf
Michał Salamon <ms055452@student.ath.edu.pl>
2022-08-31 22:18:01 +0200

    add basic NewPlayParser tests

--- BBTracker.Tests/App/Services/NewPlaysParserTests.cs
+++ BBTracker.Tests/App/Services/NewPlaysParserTests.cs
@@ -1,4 +1,5 @@
 ﻿using BBTracker.App.Services;
+using BBTracker.Model.Models;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
@@ -13,13 +14,15 @@ namespace BBTrackerTests.App.Services
     {
 
         private Guid gameId;
-        private Guid playerId;
+        private Guid player1Id;
+        private Guid player2Id;
 
         [SetUp]
         public void Setup()
         {
             gameId = Guid.NewGuid();
-            playerId = Guid.NewGuid();
+            player1Id = Guid.NewGuid();
+            player2Id = Guid.NewGuid();
         }
 
         [Test]
@@ -27,7 +30,7 @@ namespace BBTrackerTests.App.Services
         {
             string[] array = null;
             var plays = NewPlayParser.ReadPlays(array, gameId);
-            Assert.IsTrue(plays.Count() == 0);
+            Assert.IsTrue(plays.Count == 0);
         }
         [Test]
         public void EmptyArrayShouldGiveEmptyCollection()
@@ -36,13 +39,40 @@ namespace BBTrackerTests.App.Services
             var plays = NewPlayParser.ReadPlays(array, gameId);
             Assert.IsTrue(!plays.Any());
         }
-        //[Test]
-        //public void SimpleTurnoverTest()
-        //{
-        //    string[] array =
-        //        { playerId.ToString(), "false" , "sub" };
-        //    var plays = NewPlayParser.ReadPlays(array,gameId);
-        //    Assert.
-        //}
+        [Test]
+        public void SimpleFieldGoalReadProperly()
+        {
+            string[] array =
+                { player1Id.ToString(), "true", "fg" ,"3", "made", "noassist" };
+            var plays = NewPlayParser.ReadPlays(array, gameId);
+            Assert.Multiple(() =>
+            {                
+                Assert.IsTrue(plays.Count() == 1);
+                Assert.IsTrue(plays.First() is FieldGoal);
+                var play = plays.First() as FieldGoal;
+                Assert.IsTrue(play.GameId == gameId);
+                Assert.IsTrue(play.PlayerId == player1Id);
+                Assert.IsTrue(play.IsTeamB == true);
+                Assert.IsTrue(play.Points == 3);
+            });
+        }
+        [Test]
+        public void SimpleTurnoverReadProperly()
+        {
+            string[] array =
+            {
+                player1Id.ToString(), "true", "to", "nosteal"
+            };
+
+            var plays = NewPlayParser.ReadPlays(array, gameId);
+            Assert.Multiple(() =>
+            {
+                Assert.IsTrue(plays.Count == 1);
+                Assert.IsTrue(plays.First() is Turnover);
+                var turnover = plays.First() as Turnover;
+                Assert.IsTrue(turnover.GameId == gameId);
+                Assert.IsTrue(turnover?.PlayerId == player1Id);
+            });
+        }
     }
 }
